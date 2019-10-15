eea9d132698233145ecf044a4d6db0defc60ac7c
Osmar Pereira <bibliotecaosmar@gmail.com>
2019-10-15 15:38:54 -0300

    Definindo variáveis e pontos de check antes de implementar funcionalidade da pontuação bonus

--- layout/src/js/functions.js
+++ layout/src/js/functions.js
@@ -1,28 +1,45 @@
 /**
- *  System Variables
- * =====================================================================
- */
-const slotNumber = 20
-
-function slots() {
-  let slots = []
-  for(let i = 0; i < 20; i++) {
-    slots.push(['slot-', i.toString()].join())
-  }
-}
-
-let permission  = true
-
-
-/**
  *  System Functions
  * =====================================================================
  */
-const NOT   = (condition) => !condition
-const HTML  = (value, prefix = '') => ( document.getElementById(prefix + value) ) // require high order function
+const NOT				= (condition) => !condition
+const HTML  			= (value, prefix = '') => ( document.getElementById(prefix + value) )
 
-const hasInArray = (array, value) => ( true ? (array.indexOf(value) === -1) : false )
-const inRange    = (array, range) => ( range >= array.length )
+const hasInArray 		= (array, value) => ( true ? (array.indexOf(value) === -1) : false )
+const inRange    		= (array, range) => ( range >= array.length )
+
+const pushNotHaving		= (array, value) => ( array.push(value) ? (array.indexOf(value) === -1) : array
+const stringPlusNumber 	= (string, number) => ( [string, number.toString()].join() )
+
+/**
+ *  System Variables
+ * =====================================================================
+ */
+const slots = () => {
+  let slots = []
+  for(let i = 0; i < 20; i++) {
+    slots.push( stringPlusNumber('slot-', i) )
+  }
+}
+const slotNumber = () => ( slot().length )
+
+const cards = () => {
+  let card     = ['card1', 'card2', 'card3', 'card4', 'card5', 'card6', 'card7', 'card8', 'card9', 'card10']
+  let exemplar = []
+  for(let i = 0; i < slotNumber/2; i++) {
+	let y = (i*2)
+	let x = (i*2)+1
+    exemplar.push(card[y])
+	exemplar.push(card[x])
+  }
+}
+
+let chances		 = []
+let permission   = true
+let bonusStack   = []
+let bonusList    = [
+  {card: 'closeButton', bonus: 'Botão de Fechar Windows', status: 'inative', where: []},
+]
 
 /**
  *  Subprocess Functions
@@ -41,18 +58,39 @@ const addSetdown          = (card) => ( ['setdown_', card].join('') )
 const removeSetdown       = (card) => ( card.split('_')[1] )
 
 const currentPoints       = () => ( parseInt( HTML('Points').innerHTML ) )
-const addPoints           = (points, pointToGain) =>
-  ( HTML('Points').innerHTML = pointToGain(points) )
-const deductPoints        = (points, pointToLose) =>
-  ( HTML('Points').innerHTML = pointToLose(points) )
+const addPoints           = ( points, pointToGain) => ( HTML('Points').innerHTML = pointToGain(points) )
+const deductPoints        = ( points, pointToLose) => ( HTML('Points').innerHTML = pointToLose(points) )
+
+const incrementChance	  = (card) => ( chances.push(card) )
 
 /**
  *  Services(Business Rules)
  * ===================================================================== 
  */
+// Generators
+const sequencialIntNoRepeated = (range, array = []) => {
+  while(array.length < range) {
+    let x = randomNum()
+    if( NOT( hasInArray( array, x ) ) ||
+             inRange( array, range ) ) {
+      array.push(x)
+    }
+  }
+  return array
+}
+// Bonus
+const stackBonus = (bonus) => ( bonusStack.push(bonus) )
+const drawBonus  = (bonus) => ( bonusStack..remove(bonus) )
+const checkBonus = () => {
+  for(let i; i < chance.length; i++) {
+    // checa se existem 2 elementos
+	// se sim, estaca nos bonus
+  }
+}
+// Cards
 const setCards = (order) => {
-  for(let i = 0; i < slotsNumber; i++) {
-    HTML((i+1), 'slot-').className = 'setdown_slot-'+order[i]
+  for(let i = 0; i < slotNumber; i++) {
+    HTML((i+1), 'slot-').className = 'setdown_'+( cards()[order[i]] )
   }
 }
 const setCardsNotLockeds = () => {
@@ -63,6 +101,16 @@ const setCardsNotLockeds = () => {
       HTML([i]).className = addSetdown(card)
     }
   }
+}
+const lockCards = (slots) => {
+  for(let i = 0; i < slots.length; i++) {
+    card = HTML( slots[i] ).className
+    if( NOT( hasInArray( card, 'setdown_' ) &&
+             hasInArray( card, 'locked_' ) ) ) {
+      HTML( slots[i] ).className = addLocked(card)
+    }
+  }
+  HTML('FlippedCards').innerHTML = "0"
 }
 const comparator = (slots) => {
   let flippedCards = []
@@ -75,35 +123,21 @@ const comparator = (slots) => {
   }
   return true ? (cards[0] == cards[1]) : false
 }
-const lockCards = (slots) => {
-  for(let i = 0; i < slots.length; i++) {
-    card = HTML( slots[i] ).className
-    if( NOT( hasInArray( card, 'setdown_' ) &&
-             hasInArray( card, 'locked_' ) ) ) {
-      HTML( slots[i] ).className = addLocked(card)
-    }
-  }
+// Chances
+const attempt	  = () => ( HTML('attempt').innerHTML = parseInt(HTML('attempt').innerHTML)++ )
+const resetChance = () => {
+  setCardsNotLockeds()
   HTML('FlippedCards').innerHTML = "0"
 }
-const sequencialIntNoRepeated = (range, array = []) => {
-  while(array.length < range) {
-    let x = randomNum()
-    if( NOT( hasInArray( array, x ) ) ||
-             inRange( array, range ) ) {
-      array.push(x)
-    }
-  }
-  return array
-}
-const success = () => {
+const success 	  = () => {
   addPoints()
   lockCards()
 }
-const fail = () => {
+const fail 		  = () => {
   deductPoints()
   resetChance()
 }
-const checkFlips = () => {
+const checkFlips  = () => {
   if( HTML('FlippedCards').innerHTML == 2 ) {
     if( comparator() ) {
       success()
@@ -111,6 +145,7 @@ const checkFlips = () => {
       lockAll()
       time = setTimeout( ()=>{fail(); unlockAll(); clearTimeout(time)}, 2000 )
     }
+	attempt()
   }
 }
 
@@ -118,24 +153,27 @@ const checkFlips = () => {
  *  Features
  * ===================================================================== 
  */
-function start(sequence) {
+function startGame() {
+  newOrder = []
+  sequence = sequencialIntNoRepeated(slotNumber)
 
-  HTML('main-button').style.display = "none"
-}
-
-function shuffle() {
-  let newOrder = map(parseInt, sequencialIntNoRepeated(slotsNumber))
+  for(let i = 0; i < sequence.length; i++) {
+    newOrder.push(parseInt(sequence[i]))
+  }
   
   setCards(newOrder)
-
-  HTML('Screen').setdown = false
-  HTML('start').innerHTML = "Shuffle"
+  
+  HTML('main-button').style.display = "none"
 }
 
 function flip(slot) {
   if(permission) {
     card = HTML(slot, 'slot-').className
     cardsFlippeds = parseInt(HTML('FlippedCards').innerHTML)
+	
+	if( NOT( hasInArray(chances, card) ) ) {
+	  incrementChance(card)
+	}
   
     if( hasInArray(card, 'setdown_') ) {
       HTML(slot, 'slot-').className = removeSetdown(card)
@@ -143,10 +181,6 @@ function flip(slot) {
       HTML('FlippedCards').innerHTML = cardsFlippeds.toString()
     }
     checkFlips()
+	checkBonus()
   }
-}
-
-function resetChance(setCardsNotLockeds) {
-  setCardsNotLockeds()
-  HTML('FlippedCards').innerHTML = "0"
 }
